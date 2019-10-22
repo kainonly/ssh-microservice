86a315a06a4665e0d333af224de46d5bee446677
kain <kainpro@foxmail.com>
2019-10-22 23:14:57 +0800

    Submit Redis Tpl

--- src/Traits/Auth.php
+++ src/Traits/Auth.php
@@ -3,13 +3,20 @@ declare(strict_types=1);
 
 namespace Hyperf\Support\Traits;
 
+use Hyperf\HttpServer\Contract\ResponseInterface;
+
+/**
+ * Trait Auth
+ * @package Hyperf\Support\Traits
+ * @property ResponseInterface $response
+ */
 trait Auth
 {
-    protected function setLogin()
+    protected function __auth()
     {
-
-
+        return $this->response->json([
+            'error' => 1,
+            'msg' => ''
+        ]);
     }
-
-
 }
