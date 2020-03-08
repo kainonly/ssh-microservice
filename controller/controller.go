f718ee1f7123ac46d07cb9493263bfb565d68332
kain <zhangtqx@vip.qq.com>
2020-03-08 09:51:12 +0800

    更新程序结构

---
+++ controller/controller.go
@@ -0,0 +1,176 @@
+package controller
+
+import (
+	"context"
+	"encoding/base64"
+	"ssh-microservice/client"
+	"ssh-microservice/common"
+	pb "ssh-microservice/router"
+)
+
+type controller struct {
+	pb.UnimplementedRouterServer
+	client client.Client
+}
+
+func New(client *client.Client) *controller {
+	c := new(controller)
+	c.client = *client
+	return c
+}
+
+func (c *controller) Testing(ctx context.Context, params *pb.TestingParameter) (*pb.CommonResponse, error) {
+	privateKey, err := base64.StdEncoding.DecodeString(params.PrivateKey)
+	if err != nil {
+		return nil, err
+	}
+	sshClient, err := c.client.Testing(common.ConnectOption{
+		Host:       params.Host,
+		Port:       params.Port,
+		Username:   params.Username,
+		Password:   params.Password,
+		Key:        privateKey,
+		PassPhrase: []byte(params.Passphrase),
+	})
+	if err != nil {
+		return nil, err
+	}
+	defer sshClient.Close()
+	return &pb.CommonResponse{
+		Error: 0,
+		Msg:   "ok",
+	}, nil
+}
+
+func (c *controller) Put(ctx context.Context, params *pb.PutParameter) (*pb.CommonResponse, error) {
+	privateKey, err := base64.StdEncoding.DecodeString(params.PrivateKey)
+	if err != nil {
+		return nil, err
+	}
+	err = c.client.Put(params.Identity, common.ConnectOption{
+		Host:       params.Host,
+		Port:       params.Port,
+		Username:   params.Username,
+		Password:   params.Password,
+		Key:        privateKey,
+		PassPhrase: []byte(params.Passphrase),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &pb.CommonResponse{
+		Error: 0,
+		Msg:   "ok",
+	}, nil
+}
+
+func (c *controller) Exec(ctx context.Context, params *pb.ExecParameter) (*pb.ExecResponse, error) {
+	output, err := c.client.Exec(params.Identity, params.Bash)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.ExecResponse{
+		Error: 0,
+		Data:  string(output),
+	}, nil
+}
+
+func (c *controller) Delete(ctx context.Context, params *pb.DeleteParameter) (*pb.CommonResponse, error) {
+	err := c.client.Delete(params.Identity)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.CommonResponse{
+		Error: 0,
+		Msg:   "ok",
+	}, nil
+}
+
+func (c *controller) Get(ctx context.Context, params *pb.GetParameter) (*pb.GetResponse, error) {
+	data, err := c.client.Get(params.Identity)
+	if err != nil {
+		return nil, err
+	}
+	var tunnels []*pb.TunnelOption
+	for _, value := range data.Tunnels {
+		tunnels = append(tunnels, &pb.TunnelOption{
+			SrcIp:   value.SrcIp,
+			SrcPort: value.SrcPort,
+			DstIp:   value.DstIp,
+			DstPort: value.DstPort,
+		})
+	}
+	return &pb.GetResponse{
+		Error: 0,
+		Data: &pb.Information{
+			Identity:  data.Identity,
+			Host:      data.Host,
+			Port:      data.Port,
+			Username:  data.Username,
+			Connected: data.Connected,
+			Tunnels:   tunnels,
+		},
+	}, nil
+}
+
+func (c *controller) All(ctx context.Context, params *pb.AllParameter) (*pb.AllResponse, error) {
+	var keys []string
+	for key := range c.client.GetClientOptions() {
+		keys = append(keys, key)
+	}
+	return &pb.AllResponse{
+		Error: 0,
+		Data:  keys,
+	}, nil
+}
+
+func (c *controller) Lists(ctx context.Context, params *pb.ListsParameter) (*pb.ListsResponse, error) {
+	var lists []*pb.Information
+	for _, identity := range params.Identity {
+		data, err := c.client.Get(identity)
+		if err != nil {
+			return nil, err
+		}
+		var tunnels []*pb.TunnelOption
+		for _, value := range data.Tunnels {
+			tunnels = append(tunnels, &pb.TunnelOption{
+				SrcIp:   value.SrcIp,
+				SrcPort: value.SrcPort,
+				DstIp:   value.DstIp,
+				DstPort: value.DstPort,
+			})
+		}
+		lists = append(lists, &pb.Information{
+			Identity:  data.Identity,
+			Host:      data.Host,
+			Port:      data.Port,
+			Username:  data.Username,
+			Connected: data.Connected,
+			Tunnels:   tunnels,
+		})
+	}
+	return &pb.ListsResponse{
+		Error: 0,
+		Data:  nil,
+	}, nil
+}
+
+func (c *controller) Tunnels(ctx context.Context, params *pb.TunnelsParameter) (*pb.CommonResponse, error) {
+	var tunnels []common.TunnelOption
+	for _, value := range params.Tunnels {
+		tunnels = append(tunnels, common.TunnelOption{
+			SrcIp:   value.SrcIp,
+			SrcPort: value.SrcPort,
+			DstIp:   value.DstIp,
+			DstPort: value.DstPort,
+		})
+	}
+	err := c.client.SetTunnels(params.Identity, tunnels)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.CommonResponse{
+		Error: 0,
+		Msg:   "ok",
+	}, nil
+}
