faf8ed0e0e30de0e90ee1f7dfa9ad6dd307c28d6
kain <kainpro@foxmail.com>
2020-03-10 11:19:48 +0800

    Unit Test

--- client/safe_map_listener.go
+++ client/safe_map_listener.go
@@ -17,7 +17,7 @@ func newSafeMapListener() *safeMapListener {
 }
 
 func (s *safeMapListener) Clear(identity string) {
-	s.Map[identity] = make(map[string]*net.Listener)
+	delete(s.Map, identity)
 }
 
 func (s *safeMapListener) Get(identity string, addr string) *net.Listener {
@@ -29,6 +29,9 @@ func (s *safeMapListener) Get(identity string, addr string) *net.Listener {
 
 func (s *safeMapListener) Set(identity string, addr string, listener *net.Listener) {
 	s.Lock()
+	if s.Map[identity] == nil {
+		s.Map[identity] = make(map[string]*net.Listener)
+	}
 	s.Map[identity][addr] = listener
 	s.Unlock()
 }
