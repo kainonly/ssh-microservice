0d12134f09397cc91746b496dc39501fcdae5b88
kain <zhangtqx@vip.qq.com>
2020-08-26 20:56:35 +0800

    update

--- app/manage/init.go
+++ app/manage/init.go
@@ -1,6 +1,18 @@
 package manage
 
+import (
+	"golang.org/x/crypto/ssh"
+	"ssh-microservice/app/types"
+	"ssh-microservice/app/utils"
+)
+
 type ClientManager struct {
+	options       map[string]*types.SshOption
+	tunnels       map[string]*[]types.TunnelOption
+	runtime       map[string]*ssh.Client
+	localListener *utils.SyncMapListener
+	localConn     *utils.SyncMapConn
+	remoteConn    *utils.SyncMapConn
 }
 
 func NewClientManager() *ClientManager {
