30a9af8d66a65503aba31d832e23c1380c8d0974
kain <kainpro@foxmail.com>
2020-08-27 21:39:03 +0800

    update travis

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -2,7 +2,7 @@
 # Login docker
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 # Build Golang Application
-go build -o dist/ssh-microservice
+go build -o dist/ssh-microservice cmd/main.go
 # Build docker image
 docker build . -t kainonly/ssh-microservice:${TRAVIS_TAG}
 # Push docker image
