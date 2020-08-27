b05b5aa39ece72ae04b05eb313ed521c54d4315b
kain <kainpro@foxmail.com>
2020-08-27 20:58:33 +0800

    update

--- scripts/ci-default.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Login docker
-echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-# Build Golang Application
-go build -o dist/ssh-microservice
-# Build docker image
-docker build . -t kainonly/ssh-microservice:latest
-# Push docker image
-docker push kainonly/ssh-microservice:latest
