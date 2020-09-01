6f245a7dc468a83a3d30b61b2cae1ce6b1a644e0
kain <zhangtqx@vip.qq.com>
2020-09-01 19:02:24 +0800

    update

--- app/controller/lists.go
+++ app/controller/lists.go
@@ -8,35 +8,11 @@ import (
 func (c *controller) Lists(ctx context.Context, param *pb.ListsParameter) (*pb.ListsResponse, error) {
 	lists := make([]*pb.Information, len(param.Identity))
 	for index, identity := range param.Identity {
-		sshOption, err := c.manager.GetSshOption(identity)
+		information, err := c.find(identity)
 		if err != nil {
 			return c.listsErrorResponse(err)
 		}
-		client, err := c.manager.GetRuntime(identity)
-		if err != nil {
-			return c.listsErrorResponse(err)
-		}
-		tunnelOption, err := c.manager.GetTunnelOption(identity)
-		if err != nil {
-			return c.listsErrorResponse(err)
-		}
-		resultTunnelOption := make([]*pb.TunnelOption, len(tunnelOption))
-		for tIndex, option := range tunnelOption {
-			resultTunnelOption[tIndex] = &pb.TunnelOption{
-				SrcIp:   option.SrcIp,
-				SrcPort: option.SrcPort,
-				DstIp:   option.DstIp,
-				DstPort: option.DstPort,
-			}
-		}
-		lists[index] = &pb.Information{
-			Identity:  identity,
-			Host:      sshOption.Host,
-			Port:      sshOption.Port,
-			Username:  sshOption.Username,
-			Connected: string(client.ClientVersion()),
-			Tunnels:   resultTunnelOption,
-		}
+		lists[index] = information
 	}
 	return c.listsSuccessResponse(lists)
 }
